f73e6b5884ed4a10b0aa7999443304cfc0e9a3eb
“Valery” <117990439+ValeryManoilov@users.noreply.github.com>
2023-12-04 12:17:45 +0300

    add: the Education section was added to the site

---
+++ app/script.js
@@ -0,0 +1,24 @@
+const showFreeButton =  document.getElementById("showFreeButton")
+const showPaidButton =  document.getElementById("showPaidButton")
+const clearFreeButton = document.getElementById("clearFreeButton")
+const clearPaidButton = document.getElementById("clearPaidButton")
+const backFree = document.getElementById("freeEdu")
+const backPaid = document.getElementById("paidEdu")
+
+showFreeButton.addEventListener("click", FreeFunction)
+
+clearFreeButton.addEventListener("click", FreeFunction)
+
+showPaidButton.addEventListener("click", PaidFunction)
+
+clearPaidButton.addEventListener("click", PaidFunction)
+
+function FreeFunction (){
+    showFreeButton.classList.toggle("ShowFreeButton")
+    backFree.classList.toggle("FreeEduBack")
+}
+
+function PaidFunction (){
+    showPaidButton.classList.toggle("ShowPaidButton")
+    backPaid.classList.toggle("PaidEduBack")
+}
