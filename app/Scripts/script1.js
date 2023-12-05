27f4957b7c17a0368838214fdc27a28b1ab04a2a
John Doe <johndoe@example.com>
2023-12-05 23:39:02 +0300

    минус баги

--- app/Scripts/script1.js
+++ app/Scripts/script1.js
@@ -16,4 +16,18 @@ function changeImage() {
   }
   
   // Запускаем функцию changeImage каждые 5 секунд
-  setInterval(changeImage, 5000);
+  setInterval(changeImage, 5000)
+
+function showBlocks() {
+  var block1 = document.getElementById("block1");
+  var block2 = document.getElementById("block2");
+  
+  block1.style.display = "block";
+  block2.style.display = "block";
+}
+
+function removeBlock(id) {
+  var block = document.getElementById(id);
+  
+  block.style.display = "none";
+}
