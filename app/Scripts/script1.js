ef70ebf9540d003073ce64e27b67121c7fb9e5e9
John Doe <johndoe@example.com>
2023-12-07 01:02:37 +0300

    всплывающийся блок по нажатию

--- app/Scripts/script1.js
+++ app/Scripts/script1.js
@@ -16,18 +16,20 @@ function changeImage() {
   }
   
   // Запускаем функцию changeImage каждые 5 секунд
-  setInterval(changeImage, 5000)
+setInterval(changeImage, 5000)
 
-function showBlocks(){
+function ShowBlocks(){
   var block1 = document.getElementById("block1");
   var block2 = document.getElementById("block2");
-  
   block1.style.display = "block";
   block2.style.display = "block";
+
 }
 
-function removeBlock(id){
-  var block = document.getElementById(id);
-  
-  block.style.display = "none";
+function removeBlock(id1, id2){
+  var block1 = document.getElementById(id1);
+  var block2 = document.getElementById(id2);
+  block1.style.display = "none";
+  block2.style.display = "none";
+
 }
